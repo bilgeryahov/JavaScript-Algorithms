e18ad069eba11b088f2fd3f9eb22d6618139aa59
Bilger Yahov <bilger@studyportals.com>
2017-11-02 15:50:56 +0100

    Add Caesar Cipher.

---
+++ CaesarCipher/index.js
@@ -0,0 +1,57 @@
+/**
+ * @Algorithm: Caesar Cipher
+ *
+ * Build a function, which will receive two parameters.
+ * First one being a string, second one being a number.
+ *
+ * The function needs to shift all the letters given in
+ * the string, by the number given as a parameter.
+ */
+
+`use strict`;
+
+function caesarCipher(str, num) {
+	// Handle extreme cases. Length of the alphabet.
+	num = num % 26;
+	let lowerCaseString = str.toLowerCase();
+	let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	let newString = '';
+
+	for(let i = 0; i < lowerCaseString.length; i++){
+		let currentLetter = lowerCaseString[i];
+		if(currentLetter === ' '){
+			newString += currentLetter;
+			continue;
+		}
+
+		let currentIndex = alphabet.indexOf(currentLetter);
+		let newIndex = currentIndex + num;
+
+		// Repeat the alphabet, or go backwards.
+		if(newIndex > 25){
+			newIndex = newIndex - 26;
+		}
+		else if(newIndex < 0){
+			newIndex = newIndex + 26;
+		}
+
+		// Check if upper case.
+		if(str[i] === str[i].toUpperCase()){
+			newString += alphabet[newIndex].toUpperCase();
+		}
+		else{
+			newString += alphabet[newIndex];
+		}
+	}
+
+	return newString;
+}
+
+// Bqq Mggrgt
+console.log(caesarCipher('Zoo Keeper', 2));
+
+// Lsq Mkb
+console.log(caesarCipher('Big Car', -16));
+
+// TkfkCmbszd
+console.log(caesarCipher('JavaScript', -900));
