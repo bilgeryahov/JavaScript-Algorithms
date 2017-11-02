a45625315e57a1fc5a41ba2b9fd18f2fc495af3e
Bilger Yahov <bilger@studyportals.com>
2017-11-02 15:17:02 +0100

    Add IsPalindrome.

---
+++ IsPalindrome/index.js
@@ -0,0 +1,28 @@
+/**
+ * @Algorithm: Is Palindrome
+ *
+ * Build a function, which will receive a string as an argument.
+ * It needs to return true if the string is a palindrome, false
+ * otherwise.
+ *
+ * Examples of palindromes: race car, madam, i'm adam
+ */
+
+`use strict`;
+
+function isPalindrome(textString) {
+	textString = textString.toLowerCase();
+	let charactersArr = textString.split('');
+	let validCharacters = 'abcdefghiijklmnopqrstuvwxyz'.split('');
+
+	let lettersArr = [];
+	charactersArr.forEach(char => {
+		if(validCharacters.indexOf(char) !== -1){
+			lettersArr.push(char);
+		}
+	});
+
+	return(lettersArr.join('') === lettersArr.reverse().join(''));
+}
+
+console.log(isPalindrome(`Madam, I'm Adam`));
